74afe32686e09ca352c93b0baf13b10670a5f0ac
dpills <dpills@protonmail.com>
2023-10-26 13:27:52 -0400

    CI setup

---
+++ app/main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Data(BaseModel):
+    item: str
+
+
+@app.get("/data", response_model=Data)
+async def get_data() -> Data:
+    """
+    Get Data
+    """
+    return Data(item="devops")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_level="debug",
+        reload=True,
+    )
